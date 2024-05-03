549c2070658a5d6e66d8fff66adef3f2ff1f24f4
Justin Payan <jpayan@umass.edu>
2024-05-03 14:07:18 -0400

    Should be ready with the GESW alloc for expected values.

--- src/allocation_code.py
+++ src/allocation_code.py
@@ -1,6 +1,8 @@
+import cvxpy as cp
 import numpy as np
 import re
 
+from collections import Counter
 from gurobipy import Model, multidict, GRB
 
 def create_multidict(pra):
@@ -53,3 +55,33 @@ def solve_usw_gurobi(affinity_scores, covs_lb, covs_ub, loads):
     alloc = convert_to_mat(m, covs_lb.shape[0], loads.shape[0])
 
     return m.objVal, alloc
+
+
+def solve_gesw(affinity_scores, covs_lb, covs_ub, loads, groups):
+    num_groups = len(set(groups))
+    group_indicators = []
+    group_size = Counter(groups)
+    for g in range(num_groups):
+        group_indicators.append(np.zeros(affinity_scores.shape))
+    for idx, g in enumerate(groups):
+        group_indicators[g][:, idx] = 1 / group_size[g]
+
+    max_gesw_expected_alloc = cp.Variable(affinity_scores.shape, boolean=True)
+    y = cp.Variable()
+
+    flat_alloc = cp.reshape(max_gesw_expected_alloc, (max_gesw_expected_alloc.size, 1))
+    groups_stacked = cp.hstack(group_indicators)
+    flat_alloc_per_group = cp.multiply(flat_alloc, groups_stacked)
+    coeffs = cp.reshape(affinity_scores, (1, affinity_scores.size))
+    inner_prods_per_group = coeffs @ flat_alloc_per_group
+    obj = y
+
+    constr = [cp.sum(max_gesw_expected_alloc, axis=0) >= covs_lb, cp.sum(max_gesw_expected_alloc, axis=0) <= covs_ub,
+              cp.sum(max_gesw_expected_alloc, axis=1) <= loads, y >= 0, y <= inner_prods_per_group]
+
+    gesw_problem = cp.Problem(cp.Maximize(obj), constr)
+
+    gesw_problem.solve(verbose=True, solver='GUROBI')
+
+    return max_gesw_expected_alloc.value
+
--- src/compute_allocations.py
+++ src/compute_allocations.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import pickle
 import sys
-from allocation_code import solve_usw_gurobi
+from allocation_code import solve_usw_gurobi, solve_gesw
 
 dset_name_map = {"aamas1": "AAMAS1", "aamas2": "AAMAS2", "aamas3": "AAMAS3", "ads": "Advertising", "cs": "cs"}
 
@@ -14,32 +14,38 @@ def load_dset(dset_name, data_dir):
         central_estimate = np.load(os.path.join(data_dir, "AAMAS", "mu_matrix_1.npy"))
         covs_lb = 3 * np.ones(central_estimate.shape[1])
         covs_ub = 3 * np.ones(central_estimate.shape[1])
-
         loads = 10 * np.ones(central_estimate.shape[0])
+        groups = np.load(os.path.join(data_dir, "AAMAS", "groups_1.npy"))
+
     elif dset_name == "aamas2":
         central_estimate = np.load(os.path.join(data_dir, "AAMAS", "mu_matrix_2.npy"))
         covs_lb = 3 * np.ones(central_estimate.shape[1])
         covs_ub = 3 * np.ones(central_estimate.shape[1])
-
         loads = 10 * np.ones(central_estimate.shape[0])
+        groups = np.load(os.path.join(data_dir, "AAMAS", "groups_2.npy"))
+
     elif dset_name == "aamas3":
         central_estimate = np.load(os.path.join(data_dir, "AAMAS", "mu_matrix_3.npy"))
         covs_lb = 3 * np.ones(central_estimate.shape[1])
         covs_ub = 3 * np.ones(central_estimate.shape[1])
         loads = 4 * np.ones(central_estimate.shape[0])
+        groups = np.load(os.path.join(data_dir, "AAMAS", "groups_3.npy"))
+
     elif dset_name == "ads":
         central_estimate = np.load(os.path.join(data_dir, "Advertising", "mus.npy"))
         covs_lb = np.zeros(central_estimate.shape[1]) # ad campaigns have no lower bounds
         covs_ub = 100*np.ones(central_estimate.shape[1])
         loads = np.ones(central_estimate.shape[0]) # Each user impression can only have 1 ad campaign
+        groups = np.load(os.path.join(data_dir, "Advertising", "groups.npy"))
+
     elif dset_name == "cs":
         central_estimate = np.load(os.path.join(data_dir, "cs", "asst_scores.npy"))
         covs_lb = 2 * np.ones(central_estimate.shape[1])
         covs_ub = 2 * np.ones(central_estimate.shape[1])
-
         loads = 13 * np.ones(central_estimate.shape[0])
+        groups = np.load(os.path.join(data_dir, "cs", "groups.npy"))
 
-    return central_estimate, covs_lb, covs_ub, loads
+    return central_estimate, covs_lb, covs_ub, loads, groups
 
 def main(args):
     dset_name = args.dset_name
@@ -49,7 +55,7 @@ def main(args):
     data_dir = os.path.join(base_dir, "data")
     output_dir = os.path.join(base_dir, "outputs")
 
-    central_estimate, covs_lb, covs_ub, loads = load_dset(dset_name, data_dir)
+    central_estimate, covs_lb, covs_ub, loads, groups = load_dset(dset_name, data_dir)
 
     print("Loaded dataset %s, computing %s allocation" % (alloc_type, dset_name), flush=True)
 
@@ -58,8 +64,7 @@ def main(args):
     if alloc_type == "exp_usw_max":
         _, alloc = solve_usw_gurobi(central_estimate, covs_lb, covs_ub, loads)
     elif alloc_type == "exp_gesw_max":
-        pass
-        # _, alloc =
+        alloc = solve_gesw(central_estimate, covs_lb, covs_ub, loads, groups)
 
     print("Saving allocation", flush=True)
 
