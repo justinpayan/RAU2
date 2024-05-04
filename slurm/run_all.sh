5cb3136b72080ada30e3d1eb45e62bb7fd02e2f5
Justin Payan <jpayan@umass.edu>
2024-05-04 14:09:55 -0400

    Script to submit all exps

--- slurm/run_all.sh
+++ slurm/run_all.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-for DSET_NAME in ["ads" "cs" "aamas1" "aamas2" "aamas3"]; do
-  for ALLOC_TYPE in ["exp_usw_max" "exp_gesw_max"]; do
+for DSET_NAME in "ads" "cs" "aamas1" "aamas2" "aamas3"; do
+  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
     ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
     sleep 1
   done
-  for ALLOC_TYPE in ["cvar_usw" "cvar_gesw"]; do
-    for CONF_LEVEL in [0.7 0.8 0.9 0.95]; do
+  for ALLOC_TYPE in "cvar_usw" "cvar_gesw"; do
+    for CONF_LEVEL in 0.7 0.8 0.9 0.95; do
       ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
       sleep 1
     done
