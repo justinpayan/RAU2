0e6a3b5f7b2b2f58451bf9a90642804fc1efb577
Justin Payan <jpayan@umass.edu>
2024-05-07 19:40:34 -0400

    Just use torch's min

--- src/allocation_code.py
+++ src/allocation_code.py
@@ -564,7 +564,7 @@ class ComputeGroupEgalitarianQuadratic():
             # term = torch.exp(-1 * self.eta * (term1 + term2 + term3))
             # print(term)
             # term_sum = term_sum + term
-            terms[gdx] += term1.item()+term2.item()+term3.item()
+            terms[gdx] += term1+term2[0,0]+term3
             print()
         return -1*torch.min(terms)
         # soft_min = (-1.0 / self.eta) * torch.log((1.0 / self.ngroups) * term_sum)
