331732a08e34ff9301228274efb45be49d6b6f1f
Justin Payan <jpayan@umass.edu>
2024-05-17 10:28:43 -0400

    really run all

--- slurm/run_all.sh
+++ slurm/run_all.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-#for DSET_NAME in "cs" "aamas1" "aamas2" "aamas3"; do
-#  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
-#    ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
-#    sleep .1
-#  done
-#  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
-#    for CONF_LEVEL in 0.05 0.1 0.2 0.3; do
-#      ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
-#      sleep .1
-#    done
-#  done
-#done
+for DSET_NAME in "cs" "aamas1" "aamas2" "aamas3"; do
+  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
+    ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
+    sleep .1
+  done
+  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
+    for CONF_LEVEL in 0.05 0.1 0.2 0.3; do
+      ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
+      sleep .1
+    done
+  done
+done
 
 for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
   for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
