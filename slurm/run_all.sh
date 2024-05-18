4a8f24408fe4fabdac156708e22a19161ba84920
Justin Payan <jpayan@umass.edu>
2024-05-17 22:51:20 -0400

    do all one more time

--- slurm/run_all.sh
+++ slurm/run_all.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
 
-#for DSET_NAME in "cs", "aamas1" "aamas2" "aamas3"; do
-#  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
-#    ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
-#    sleep .1
-#  done
-#  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
-#    for CONF_LEVEL in 0.05 0.1 0.2 0.3; do
-#      ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
-#      sleep .1
-#    done
-#  done
-#done
-
 for DSET_NAME in "cs", "aamas1" "aamas2" "aamas3"; do
-  ALLOC_TYPE="adv_gesw"
-  for CONF_LEVEL in 0.05 0.1 0.2 0.3; do
-    ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
+  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
+    ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
     sleep .1
   done
+  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
+    for CONF_LEVEL in 0.05 0.1 0.2 0.3; do
+      ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
+      sleep .1
+    done
+  done
 done
+
 
 # "cs"
 #
