b57f68b335fb83158c0bc043267c813eac167163
Justin Payan <jpayan@umass.edu>
2024-05-18 16:09:46 -0400

    Compute metrics for low cvar level noise tests

--- slurm/run_all_noise_metric_calcs.sh
+++ slurm/run_all_noise_metric_calcs.sh
@@ -2,14 +2,14 @@
 
 for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
   for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
-      for NOISE_MULTIPLIER in 1.0 100.0 200.0 300.0 400.0 500.0 600.0 700.0 800.0 900.0 1000.0; do
+      for NOISE_MULTIPLIER in 1.0 10.0 20.0 30.0 40.0 50.0 60.0 70.0 80.0 90.0 100.0; do
         ./submit_noise_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0 ${NOISE_MULTIPLIER}
         sleep .1
       done
   done
   for ALLOC_TYPE in "cvar_usw" "cvar_gesw"; do
-    CONF_LEVEL=0.3
-    for NOISE_MULTIPLIER in 1.0 100.0 200.0 300.0 400.0 500.0 600.0 700.0 800.0 900.0 1000.0; do
+    CONF_LEVEL=0.01
+    for NOISE_MULTIPLIER in 1.0 10.0 20.0 30.0 40.0 50.0 60.0 70.0 80.0 90.0 100.0; do
       ./submit_noise_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL} ${NOISE_MULTIPLIER}
       sleep .1
     done
