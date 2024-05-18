01649cb8f7bacfd37c03350735f71c4ac5ab8f44
Justin Payan <jpayan@umass.edu>
2024-05-17 21:19:32 -0400

    Redo the aamas metrics calculations

--- slurm/run_all_metric_calcs.sh
+++ slurm/run_all_metric_calcs.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
+#
+#for DSET_NAME in "cs" "aamas1" "aamas2" "aamas3"; do
+#  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
+#    ./submit_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
+#    sleep .1
+#  done
+#  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
+#    for CONF_LEVEL in 0.05 0.1 0.2 0.3; do
+#      ./submit_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
+#      sleep .1
+#    done
+#  done
+#done
 
-for DSET_NAME in "cs" "aamas1" "aamas2" "aamas3"; do
+for DSET_NAME in "aamas1" "aamas2" "aamas3"; do
   for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
     ./submit_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
     sleep .1
@@ -13,15 +26,16 @@ for DSET_NAME in "cs" "aamas1" "aamas2" "aamas3"; do
   done
 done
 
-for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
-  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
-    ./submit_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
-    sleep .1
-  done
-  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
-    for CONF_LEVEL in 0.05 0.1 0.2 0.3; do
-      ./submit_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
-      sleep .1
-    done
-  done
-done
+#
+#for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
+#  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
+#    ./submit_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
+#    sleep .1
+#  done
+#  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
+#    for CONF_LEVEL in 0.05 0.1 0.2 0.3; do
+#      ./submit_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
+#      sleep .1
+#    done
+#  done
+#done
