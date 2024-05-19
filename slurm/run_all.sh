f88d46766e6e5758cb33a8337acf84a66fe514c0
Justin Payan <jpayan@umass.edu>
2024-05-18 22:59:04 -0400

    Redo all the exps with 400 pairs (800)

--- slurm/run_all.sh
+++ slurm/run_all.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 
-for DSET_NAME in "cs" "aamas1" "aamas2" "aamas3"; do
+for DSET_NAME in "aamas1" "aamas2" "aamas3" "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
   for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
     ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
     sleep .1
   done
-  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
-    for CONF_LEVEL in 0.01 0.3; do
-      ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
-      sleep .1
-    done
+  for ALLOC_TYPE in "cvar_usw" "cvar_gesw"; do
+    CONF_LEVEL=0.01
+    ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
+    sleep .1
+  done
+  for ALLOC_TYPE in "adv_usw" "adv_gesw"; do
+    CONF_LEVEL=0.3
+    ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
+    sleep .1
   done
 done
 
-for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
-  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
-    ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
-    sleep .1
-  done
-  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
-    for CONF_LEVEL in 0.01 0.3; do
-      ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
-      sleep .1
-    done
-  done
-done
+#for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
+#  for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
+#    ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0
+#    sleep .1
+#  done
+#  for ALLOC_TYPE in "cvar_usw" "cvar_gesw" "adv_usw" "adv_gesw"; do
+#    for CONF_LEVEL in 0.01 0.3; do
+#      ./submit_compute_allocations.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL}
+#      sleep .1
+#    done
+#  done
+#done
 
 #for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
 #  for ALLOC_TYPE in "adv_gesw"; do
