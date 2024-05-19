88bce15e4026e0c89672eb7aabfd7e6dc16dfe33
Justin Payan <jpayan@umass.edu>
2024-05-18 23:45:04 -0400

    Do less for the noised runs

--- slurm/run_all_noise_metric_calcs.sh
+++ slurm/run_all_noise_metric_calcs.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-for seed in {1..20}; do
+for seed in {1..10}; do
   for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
     for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
-        for NOISE_MULTIPLIER in 1.0 10.0 20.0 30.0 40.0 50.0 60.0 70.0 80.0 90.0 100.0; do
+        for NOISE_MULTIPLIER in 1.0 20.0 40.0 60.0 80.0; do
           ./submit_noise_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0 ${NOISE_MULTIPLIER} $SEED
           sleep .1
         done
     done
     for ALLOC_TYPE in "cvar_usw" "cvar_gesw"; do
       CONF_LEVEL=0.01
-      for NOISE_MULTIPLIER in 1.0 10.0 20.0 30.0 40.0 50.0 60.0 70.0 80.0 90.0 100.0; do
+      for NOISE_MULTIPLIER in 1.0 20.0 40.0 60.0 80.0; do
         ./submit_noise_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL} ${NOISE_MULTIPLIER} $SEED
         sleep .1
       done
