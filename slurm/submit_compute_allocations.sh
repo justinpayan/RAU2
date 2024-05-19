a9f081e0c39d9a3df22a3183ff4f2fac50410068
Justin Payan <jpayan@umass.edu>
2024-05-18 23:35:43 -0400

    Run everything 20 times

--- slurm/submit_compute_allocations.sh
+++ slurm/submit_compute_allocations.sh
@@ -5,5 +5,6 @@ module load gurobi/1001
 DSET_NAME=$1
 ALLOC_TYPE=$2
 CONF_LEVEL=$3
+SEED=$4
 
 python ../src/compute_allocations.py --dset_name $DSET_NAME --alloc_type $ALLOC_TYPE --conf_level $CONF_LEVEL --seed ${SEED}
