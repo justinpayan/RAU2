231b8dbb9fc996eae24fcff7ff6a4360e57bb554
Justin Payan <jpayan@umass.edu>
2024-05-20 10:46:43 -0400

    run noise tests with more noise

--- slurm/run_all_noise_metric_calcs.sh
+++ slurm/run_all_noise_metric_calcs.sh
@@ -3,14 +3,14 @@
 for SEED in {1..5}; do
   for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
     for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
-        for NOISE_MULTIPLIER in 1.0 2.0 4.0 6.0 8.0; do
+        for NOISE_MULTIPLIER in 1.0 20.0 40.0 60.0 80.0; do
           ./submit_noise_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} 0 ${NOISE_MULTIPLIER} ${SEED}
           sleep .1
         done
     done
     for ALLOC_TYPE in "cvar_usw" "cvar_gesw"; do
       CONF_LEVEL=0.01
-      for NOISE_MULTIPLIER in 1.0 2.0 4.0 6.0 8.0; do
+      for NOISE_MULTIPLIER in 1.0 20.0 40.0 60.0 80.0; do
         ./submit_noise_calc_metrics.sbatch ${DSET_NAME} ${ALLOC_TYPE} ${CONF_LEVEL} ${NOISE_MULTIPLIER} ${SEED}
         sleep .1
       done
