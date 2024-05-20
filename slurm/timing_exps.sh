e5bbba6c2642de5c94b42c5ac2d0c2ce18613bc5
Justin Payan <jpayan@umass.edu>
2024-05-19 21:05:58 -0400

    redo the time tests with correct flag

--- slurm/timing_exps.sh
+++ slurm/timing_exps.sh
@@ -7,4 +7,4 @@ ALLOC_TYPE=$2
 CONF_LEVEL=$3
 ADV_METHOD=$4
 
-python ../src/compute_allocations.py --dset_name $DSET_NAME --alloc_type $ALLOC_TYPE --conf_level $CONF_LEVEL --adv_usw_method $ADV_METHOD --mode "time"
+python ../src/compute_allocations.py --dset_name $DSET_NAME --alloc_type $ALLOC_TYPE --conf_level $CONF_LEVEL --adv_method $ADV_METHOD --mode "time"
