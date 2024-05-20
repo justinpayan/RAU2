0b2f775111e8df70f1d06eaa3158253b411e88f4
Justin Payan <jpayan@umass.edu>
2024-05-20 09:31:35 -0400

    try again

--- slurm/run_all_noise_metric_calcs.sh
+++ slurm/run_all_noise_metric_calcs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for seed in {1..5}; do
+for SEED in {1..5}; do
   for DSET_NAME in "gauss_aamas1" "gauss_aamas2" "gauss_aamas3"; do
     for ALLOC_TYPE in "exp_usw_max" "exp_gesw_max"; do
         for NOISE_MULTIPLIER in 1.0 20.0 40.0 60.0 80.0; do
